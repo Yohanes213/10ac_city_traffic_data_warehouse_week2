3af76252a59c6a313ba114d1cd52e562192c6462
Yohanes Teshome Kebede <99422479+Yohanes213@users.noreply.github.com>
2024-05-04 19:23:02 +0300

    Update airflow_dbt_executor.py

--- dags/airflow_dbt_executor.py
+++ dags/airflow_dbt_executor.py
@@ -1,29 +1,48 @@
+"""
+DBT DAG: A Directed Acyclic Graph (DAG) for running dbt (data build tool) tasks.
+
+This DAG automates the execution of dbt tasks for processing traffic data.
+
+Tasks:
+1. dbt_debug: Command to run 'dbt debug' for debug checks on the dbt project.
+2. dbt_run: Command to execute 'dbt run' for processing data in the dbt project.
+
+"""
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
-with DAG(
+# Define DAG
+dag = DAG(
     dag_id="dbt_dag",
-    schedule_interval="@daily", 
-    start_date=datetime(2024, 5, 1), 
-    catchup=False  
-) as dag:
+    schedule_interval="@daily",
+    start_date=datetime(2024, 5, 1),
+    catchup=False,
+    default_args={
+        "owner": "Airflow",
+        "depends_on_past": False,
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+    },
+)
 
-    dbt_debug = BashOperator(
-        task_id="dbt_debug",
-        bash_command="dbt debug",
-        cwd="dbt/traffic_data",
-        retries=1,  
-        retry_delay=timedelta(minutes=5),
-    )
+# Define tasks
+dbt_debug = BashOperator(
+    task_id="dbt_debug",
+    bash_command="dbt debug",
+    cwd="dbt/traffic_data",
+    dag=dag,
+)
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
-        bash_command="dbt run",
-        cwd="dbt/traffic_data",
-        retries=3,
-        retry_delay=timedelta(minutes=5),
-    )
+dbt_run = BashOperator(
+    task_id="dbt_run",
+    bash_command="dbt run",
+    cwd="dbt/traffic_data",
+    dag=dag,
+)
 
-    dbt_debug >> dbt_run  
-
+# Set task dependencies
+dbt_debug >> dbt_run
